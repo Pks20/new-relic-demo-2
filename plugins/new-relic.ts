7776d19dbe01f6794fadb765626431c164c8ced7
Shubham Gupta <shubhamedu.01@gmail.com>
2025-06-10 18:00:14 +0530

    chore: test session

--- plugins/new-relic.ts
+++ plugins/new-relic.ts
@@ -3,10 +3,10 @@ import { BrowserAgent } from '@newrelic/browser-agent/loaders/browser-agent';
 export default defineNuxtPlugin(() => {
   const options = {
     init: {
-      distributed_tracing: { enabled: true },
       ajax: { deny_list: [] }, // Ensure all requests are captured
       error_collection: { enabled: true }, // Enable automatic error tracking
-      page_view_timing: { enabled: true } // Required for PageView events
+      page_view_timing: { enabled: true },
+      session_replay: {enabled:true,block_selector:'',mask_text_selector:'*',sampling_rate:10.0,error_sampling_rate:100.0,mask_all_inputs:true,collect_fonts:true,inline_images:false,inline_stylesheet:true,fix_stylesheets:true,preload:true,mask_input_options:{}},distributed_tracing:{enabled:true},privacy:{cookies_enabled:true}// Required for PageView events
     },
     info: {
       applicationID: '1134593720',
@@ -27,7 +27,7 @@ export default defineNuxtPlugin(() => {
   // Add custom error handler for Nuxt route errors
 
   const router = useRouter();
-        agent.api.noticeError("SHUBHAM");
+        agent.api.noticeError("SHUBHAM MITHHA");
         agent.api.addPageAction('Route404', {
           route: router.currentRoute.value.path,
           referrer: document.referrer
