e934cda24279425df56c8260bda1ce19462ecec4
Shubham Gupta <shubhamedu.01@gmail.com>
2025-06-10 19:00:03 +0530

    chore: test randome pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>
-        page 1
-    </div>
+  <div>
+    page 1
+    <nuxt-link :to="randomRoute" @click.native="generateNewRoute">
+      Go to random route
+    </nuxt-link>
+  </div>
 </template>
+
+<script lang="ts" setup>
+const randomRoute = ref(generateRandomPath());
+
+function generateRandomPath() {
+  // Generate 9-digit random number path
+  return `/${Math.random().toString().slice(2, 11)}`;
+}
+function generateNewRoute() {
+  randomRoute.value = generateRandomPath();
+}
+</script>
